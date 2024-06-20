808f63e37d22f0ea701cdd16fd8f66613256db48
FangtianyuanHu <fh2160@nyu.edu>
2024-06-20 04:59:17 -0400

    ok

---
+++ for_loop_version.py
@@ -0,0 +1,15 @@
+def get_starting_number():
+    number_beer = input("How many bottles of beer on the wall? ")
+    while number_beer.isnumeric() == False or int(number_beer)<1:
+        print("enter a correct number above or equal 1.")
+        number_beer = input("How many bottles of beer on the wall? ")
+    return int(number_beer)
+
+def sing(singing):
+    for n in range(singing, 0, -1):
+        print(f"{n} bottle{'s' if n > 1 else ''} of beer on the wall, {n} bottle{'s' if n > 1 else ''} of beer.")
+        if n > 1:
+            print(f"Take one down, pass it around, {n-1} bottle{'s' if n-1 > 1 else ''} of beer on the wall.")
+        else:
+            print("Take it down, pass it around, no more bottles of beer on the wall!")
+
---
+++ while_loop_accumulator_version.py
@@ -0,0 +1,18 @@
+def get_starting_number():
+    number_beer = input("How many bottles of beer on the wall? ")
+    while number_beer.isnumeric() == False or int(number_beer)<1:
+        print("enter a correct number above or equal 1.")
+        number_beer = input("How many bottles of beer on the wall? ")
+    return int(number_beer)
+
+def sing(singing):
+    n = singing
+    
+    while n > 0:
+        print(f"{n} bottle{'s' if n > 1 else ''} of beer on the wall, {n} bottle{'s' if n > 1 else ''} of beer.")
+        if n > 1:
+            print(f"Take one down, pass it around, {n-1} bottle{'s' if n-1 > 1 else ''} of beer on the wall.")
+        else:
+            print("Take it down, pass it around, no more bottles of beer on the wall!")
+            ssing = False
+        n -= 1
